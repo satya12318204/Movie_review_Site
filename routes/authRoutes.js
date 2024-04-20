3c80fb70b84d478e3c7fd1a343b906a0207ff550
satya perisetty <125467928+satya123182@users.noreply.github.com>
2024-04-20 16:29:46 +0530

    Revert "aes"
    This reverts commit 21a56086f1d2454cfdecf52ec8bf8c8c02212cbd.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const verifyToken = require("../middleware/authMiddleware");
-const User = require("../models/User"); 
+const User = require("../models/User"); // Corrected import statement
 
 router.get('/login', (req, res) => {
     res.render('login.ejs');
@@ -14,9 +14,13 @@ router.get('/signup', (req, res) => {
 
 router.get("/index", verifyToken, async (req, res) => {
     try {
+        // Get user ID from the decoded JWT token
         const userId = req.user.id;
-        console.log("userId: " + userId);
-        const user = await User.findById(userId); 
+
+        // Fetch user details from the database based on the ID
+        const user = await User.findById(userId); // Corrected variable name to avoid conflict with imported module
+
+        // Render the index page with the user's details
         res.render("index", { name: user.name, email: user.email });
     } catch (error) {
         console.error(error);
