e788f56e90b027ee50b5b3d5c16c877b3f0178ef
satya perisetty <125467928+satya123182@users.noreply.github.com>
2024-04-20 17:43:24 +0530

    finally

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,24 +1,4 @@
-/*const jwt = require("jsonwebtoken");
 
-function verifyToken(req, res, next) {
-  if (req.cookies.jwt) {
-    const token = req.cookies.jwt;
-    jwt.verify(token, "your_secret_key", (err, decodedToken) => {
-      if (err) {
-        console.error(err);
-        res.redirect("/login");
-      } else {
-        console.log("Decoded token:", decodedToken);
-        req.user = decodedToken; // Set the decoded user information in the request object
-        next();
-      }
-    });
-  } else {
-    res.redirect("/login");
-  }
-}
-
-module.exports = verifyToken;*/
 const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
